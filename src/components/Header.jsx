a0f6069eb9a4897b898661339aff3c4d99ec3d5b
JarikiSungnyung <hyuk0496@gmail.com>
2024-04-12 08:54:27 +0900

    feat: Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import "../styles/header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="left-side">
+          <h1>Conhub</h1>
+        </div>
+        <div className="search-bar">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input placeholder="어떤 대회를 찾으세요?" type="text" />
+          <FontAwesomeIcon icon={faXmark} />
+        </div>
+        <div className="right-side">
+          <p>Login</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
